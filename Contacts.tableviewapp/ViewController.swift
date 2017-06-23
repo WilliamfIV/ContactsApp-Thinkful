d86bd786e1b1646c1723a2bc53533a4d292ec8b8
William Henry Fulton IV <will.fulton95@gmail.com>
2017-06-23 17:46:02 -0400

    Initial Commit

---
+++ Contacts.tableviewapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts.tableviewapp
+//
+//  Created by William H Fulton IV on 6/23/17.
+//  Copyright © 2017 IVdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
