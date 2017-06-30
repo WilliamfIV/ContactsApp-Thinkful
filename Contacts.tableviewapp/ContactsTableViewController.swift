7c3f194a4b7b06d348c5a1ebdac7602349dba254
William Henry Fulton IV <will.fulton95@gmail.com>
2017-06-30 01:30:34 -0400

    code for rows rearranging rows has been added, edit bar button has been added, still unable to interact screen when rearranging cell order.

--- Contacts.tableviewapp/ContactsTableViewController.swift
+++ Contacts.tableviewapp/ContactsTableViewController.swift
@@ -8,42 +8,83 @@
 
 import UIKit
 
+
+//class for TableView that contains contacts
+
 class ContactsTableViewController: UITableViewController {
    
+    
+    //  Array propery for contacts
+    
     var contacts:[Contact] = []
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.contacts.append(Contact(name: "Jil", phoneNumber: "7033047095"))
-        self.contacts.append(Contact(name: "Jason", phoneNumber: "70403049304"))
-        self.contacts.append(Contact(name: "Robby", phoneNumber: "6969696996969"))
+        
+        // UIBarButtonItem class - Creating a edit button to rearrange order of contacts
+        
+        let moveButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(ContactsTableViewController.toggleEdit))
+        navigationItem.leftBarButtonItem = moveButton
+       
+      
+        
+        //  contacts added to the array of contacts
+        
+        let jenny = Contact(phoneNumber: "867-5309")
+        let rich = Contact(name: "Rich", phoneNumber: "888-888-8888")
+        let mindy = Contact(name: "Mindy")
+        
+        self.contacts.append(jenny)
+        self.contacts.append(rich)
+        self.contacts.append(mindy)
     }
 
+   
+    // allow editing contacts
+    
+    func toggleEdit() {
+        tableView.setEditing(!tableView.isEditing, animated: true)
+    }
+    
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-  
     }
+
+    
+    
 
     // MARK: - Table view data source
-
+    
+    
+    // All contacts will be contained in one section
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
-   
     }
 
+   
+    
+    
+    
+    //Creates a row for each contact... Uses the count property of the array.
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.contacts.count
-        //Creates a row for each contact... Uses the count PROPERTY of the array.
     }
-
+    
+   
+   
+    
+    //This populates each cell with a contact name..... "cellForRowAt indexPath"
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         let contact = self.contacts[indexPath.row]
-        
        
         if let name = contact.name {
             cell.textLabel?.text = name
@@ -52,61 +93,73 @@ class ContactsTableViewController: UITableViewController {
         }
         
         return cell
-        
-    // This populates each cell with a contact name..... "cellForRowAt indexPath"
     }
     
-
-
-    /*
-    // Override to support conditional editing of the table view.
+   
+   
+    // Allows cells to be edited
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+   
+    
+    
+    
+    
+    //Deleting contacts and re-ordering cells
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            self.contacts.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }   
     }
-    */
+    
+  
 
-    /*
+   
     // Override to support rearranging the table view.
+    
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        let contactMoving = contacts.remove(at: fromIndexPath.row)
+        contacts.insert(contactMoving, at: to.row)
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+ 
+   
+    
+    //getting rid of the delete symbol 'minus symbol'
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle{
+        if tableView.isEditing {
+            return .none
+        } else {
+            return .delete
+        }
     }
-    */
+    //getting rid of indentation in row
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
 
     
     // MARK: - Navigation
-
+    
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let indexPath = self.tableView.indexPath(for: sender as! UITableViewCell)!
         let contact = self.contacts[indexPath.row]
         let destination = segue.destination as! DetailViewController
         destination.contact = contact
-        
     }
 }
 
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+
 
 
 
