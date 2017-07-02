2247dca9974f686ae530f3fac628b496ba2d6639
William Henry Fulton IV <will.fulton95@gmail.com>
2017-07-02 17:57:05 -0400

    completed requirements for thankful tableview assignment

--- Contacts.tableviewapp/ContactsTableViewController.swift
+++ Contacts.tableviewapp/ContactsTableViewController.swift
@@ -9,67 +9,75 @@
 import UIKit
 
 
+
+
 //class for TableView that contains contacts
 
 class ContactsTableViewController: UITableViewController {
    
-    
     //  Array propery for contacts
     
     var contacts:[Contact] = []
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         // UIBarButtonItem class - Creating a edit button to rearrange order of contacts
         
         let moveButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(ContactsTableViewController.toggleEdit))
         navigationItem.leftBarButtonItem = moveButton
        
-      
+
         
-        //  contacts added to the array of contacts
-        
-        let jenny = Contact(phoneNumber: "867-5309")
-        let rich = Contact(name: "Rich", phoneNumber: "888-888-8888")
-        let mindy = Contact(name: "Mindy")
-        
-        self.contacts.append(jenny)
-        self.contacts.append(rich)
-        self.contacts.append(mindy)
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ContactsTableViewController.addContact))
+        navigationItem.rightBarButtonItem = addButton
+    
     }
 
-   
+    
+    
+    
+    
+
     // allow editing contacts
     
     func toggleEdit() {
-        tableView.setEditing(!tableView.isEditing, animated: true)
+        self.tableView.setEditing(!tableView.isEditing, animated: true)
     }
+  
     
-
-
-
+    //create a new contact 'new contact', add it to the array, calculates the new index path by subtracting 1 from the total number of objects in contacts(since index path starts at 0, then insert new row
+    func addContact() {
+        let newContact = Contact(name: "New Contact")
+        self.contacts.append(newContact)
+        let newIndexPath = IndexPath(row: self.contacts.count - 1, section: 0)
+        self.tableView.insertRows(at: [newIndexPath], with: .automatic )
+    }
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // MARK: - Table view data source
-    
     
     // All contacts will be contained in one section
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+  override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
-
     
     //Creates a row for each contact... Uses the count property of the array.
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.contacts.count
     }
-    
-   
-   
     
     //This populates each cell with a contact name..... "cellForRowAt indexPath"
     
@@ -83,29 +91,22 @@ class ContactsTableViewController: UITableViewController {
         } else {
             cell.textLabel?.text = "no name"
         }
-        
         return cell
     }
     
-   
-   
     // Allows cells to be edited
     
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
    
-    
-    
-    
     //Deleting contacts and re-ordering cells
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             self.contacts.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-           
-        }   
+        }
     }
     
     // Override to support rearranging the table view.
@@ -113,15 +114,8 @@ class ContactsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
         let contactMoving = contacts.remove(at: fromIndexPath.row)
         contacts.insert(contactMoving, at: to.row)
-        
     }
-    
-    
-    
-    
-    
-    
-    
+
     //getting rid of the delete symbol 'minus symbol'
     
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle{
@@ -137,6 +131,15 @@ class ContactsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
         return false
     }
+
+    
+    //this ensure that the model object will change when the tableview is offscreen 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
     
     
     // MARK: - Navigation
@@ -144,6 +147,7 @@ class ContactsTableViewController: UITableViewController {
     // Get the new view controller using segue.destinationViewController.
     // Pass the selected object to the new view controller.
     
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let indexPath = self.tableView.indexPath(for: sender as! UITableViewCell)!
         let contact = self.contacts[indexPath.row]
--- DetailViewController.swift
+++ DetailViewController.swift
@@ -8,30 +8,46 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITextFieldDelegate {
+//A text field calls the methods of its delegate in response to important changes... UITextFieldDelegate must be added as a SuperClass
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
     
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var phoneNumberLabel: UILabel!
     var contact: Contact?
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.nameTextField.delegate = self
+        self.phoneNumberTextField.delegate = self
+        
         if let contact = self.contact {
             if let name = contact.name {
-               self.nameLabel.text = name
+               self.nameTextField.text = name
         }
             if let phoneNumber = contact.phoneNumber {
-                self.phoneNumberLabel.text = phoneNumber
+                self.phoneNumberTextField.text = phoneNumber
         }
     }
 }
-
-  
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
     }
+
+
+
+// when a user is done typing in a text field, the text is assignent to the 'contact' object
+//the code in the func checks if the text is name or phone number and save it to the appropriate property
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.nameTextField {
+            self.contact?.name = textField.text
+        } else if textField == self.phoneNumberTextField {
+            self.contact?.phoneNumber = textField.text
+        }
+    }
+    
+    
+
 }
